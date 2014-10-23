9c1fd319d882ba81e42a9a6fc8ae5f5939fdc5ff
ikarosouza <ikarolp@hotmail.com>
2014-10-23 17:36:52 -0300

    Create Views

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009220454) do
+ActiveRecord::Schema.define(version: 20141023021223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ejs", force: true do |t|
+    t.string   "formal_name"
+    t.string   "cnpj"
+    t.string   "site"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
