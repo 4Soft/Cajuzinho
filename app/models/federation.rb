d48916309d0b04c3a24297ee7450dfebb6c4b284
Lucas Limeira <lucasalveslm@gmail.com>
2014-10-31 08:21:31 -0300

    added polymorphic association to federation, assessor and ej models

--- app/models/federation.rb
+++ app/models/federation.rb
@@ -2,5 +2,7 @@ class Federation < ActiveRecord::Base
 	has_many :JuniorEnterprises
 	validates :formal_name, presence: true,
 					   	length: {minimum: 5}
+	has_one :user, as: :role, dependent: :destroy
+	accepts_nested_attributes_for :user
 
 end
