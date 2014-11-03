c71b8db9ebdc8ee36cc4976d4a80bbab012762de
Raquel <kelopes2@gmail.com>
2014-11-03 17:45:22 +0000

    Change of message

--- app/models/ej.rb
+++ app/models/ej.rb
@@ -1,9 +1,9 @@
 class Ej < ActiveRecord::Base
 	belongs_to :Federation
 	validates :formal_name,
-  :presence => {:message => "O campo não pode ser vazio" }
+  :presence => {:message => "Campo vazio inválido" }
 	validates :cnpj,
-		:presence => {:message => "O campo não pode ser vazio" }
+  :presence => {:message => "Campo vazio inválido" }
 
 	has_one :user, as: :role, dependent: :destroy
 	accepts_nested_attributes_for :user
